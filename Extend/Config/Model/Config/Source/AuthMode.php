e8e6216c21747178fe5694c261b44c0e289c5666
Christopher Delgado <christopherdel98@gmail.com>
2019-05-20 17:49:14 -0600

    sections authentication and advance configurations finished

---
+++ Extend/Config/Model/Config/Source/AuthMode.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Extend\Config\Model\Config\Source;
+
+use Magento\Framework\Option\ArrayInterface;
+
+class AuthMode implements ArrayInterface
+{
+
+    /**
+     * Return array of options as value-label pairs
+     *
+     * @return array Format: array(array('value' => '<value>', 'label' => '<label>'), ...)
+     */
+    public function toOptionArray()
+    {
+        return [
+            ['value' => 'live', 'label' => __('Live')],
+            ['value' => 'sandbox', 'label' => __('Sandbox')]
+        ];
+    }
+}
---
+++ Extend/Config/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Extend_Config',
+    __DIR__
+);
