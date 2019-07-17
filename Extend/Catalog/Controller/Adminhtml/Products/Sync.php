e4028c2935b3f2e341787bc6d43c23091d6fe14f
Cesar Flores <cesar.flores@gammapartners.com>
2019-07-17 12:57:25 -0600

    [HEL-154] Functionality to cancel product sync

--- Extend/Catalog/Controller/Adminhtml/Products/Sync.php
+++ Extend/Catalog/Controller/Adminhtml/Products/Sync.php
@@ -6,67 +6,70 @@ use Magento\Backend\App\Action;
 use Magento\Framework\Controller\Result\Json as JsonResult;
 use Magento\Framework\Controller\ResultFactory;
 use Extend\Catalog\Model\SyncProcess;
+use Magento\Framework\Filesystem\File\Read;
 use Psr\Log\LoggerInterface;
 use Extend\Catalog\Model\ProductsCollection;
+use Magento\Framework\Module\Dir\Reader;
+use Magento\Framework\File\Csv;
 
 class Sync extends Action
 {
-    const MAX_PRODUCTS_BATCH = 250;
+    const MAX_PRODUCTS_BATCH = 20;
     protected $_publicActions = ['extend/products/sync'];
     protected $resultFactory;
     protected $logger;
     protected $syncProcess;
     protected $productsCollection;
+    protected $fileCsv;
+    protected $moduleReader;
 
     public function __construct(
         Action\Context $context,
         ResultFactory $resultFactory,
         LoggerInterface $logger,
         SyncProcess $syncProcess,
-        ProductsCollection $productsCollection
+        ProductsCollection $productsCollection,
+        Csv $fileCsv,
+        Reader $moduleReader
+
     )
     {
         $this->productsCollection = $productsCollection;
         $this->resultFactory = $resultFactory;
         $this->logger = $logger;
         $this->syncProcess = $syncProcess;
+        $this->fileCsv = $fileCsv;
+        $this->moduleReader = $moduleReader;
+
         parent::__construct($context);
     }
 
     public function execute()
     {
-        $result = $this->resultFactory->create(ResultFactory::TYPE_JSON);
         $storeProducts = $this->productsCollection->getProducts();
 
-        try{
-            $numOfBatches = ceil(sizeof($storeProducts)/self::MAX_PRODUCTS_BATCH);
-            $i = 0;
-            while ($numOfBatches>0){
-                if($numOfBatches === 1){
-                    $productsInBatch = array_slice($storeProducts,$i*self::MAX_PRODUCTS_BATCH);
-                }else{
-                    $productsInBatch = array_slice($storeProducts,$i*self::MAX_PRODUCTS_BATCH,self::MAX_PRODUCTS_BATCH);
-                }
-                $this->syncProcess->sync($productsInBatch);
-                $numOfBatches--;
-                $i++;
-                sleep(0.75);
-            }
-            $code = 200;
-            $result = $this->prepareResult($result, $code);
-            $this->logger->info('Products Successfully Synchronized');
-            return $result;
-        }catch(Exception $e){
-            $msg = __($e->getMessage());
-            $code = 500;
-            $result = $this->prepareResult($result, $code, ['msg' => $msg]);
-            return $result;
-        }
-    }
+        try {
+            $totalBatches = ceil(sizeof($storeProducts) / self::MAX_PRODUCTS_BATCH);
+            $currentBatch = (int)$this->getRequest()->getParam('currentBatchesProcessed');
 
-    protected function prepareResult(JsonResult $result, int $code, array $data = [])
-    {
-        $result->setHttpResponseCode($code);
-        $result->setData($data);
+            if ($currentBatch === $totalBatches) {
+                $productsInBatch = array_slice($storeProducts, $currentBatch * self::MAX_PRODUCTS_BATCH);
+            } else {
+                $productsInBatch = array_slice($storeProducts, $currentBatch * self::MAX_PRODUCTS_BATCH, self::MAX_PRODUCTS_BATCH);
+            }
+            $this->syncProcess->sync($productsInBatch);
+
+            $currentBatch++;
+
+            $data = [
+                'totalBatches' => (int)$totalBatches,
+                'currentBatchesProcessed' => (int)$currentBatch
+                ];
+            return $this->resultFactory->create(ResultFactory::TYPE_JSON)->setHttpResponseCode(200)->setData($data);
+
+        } catch (Exception $e) {
+            $data = ['msg' => $e->getMessage()];
+            return $this->resultFactory->create(ResultFactory::TYPE_JSON)->setHttpResponseCode(500)->setData($data);
+        }
     }
 }
--- Extend/Warranty/view/adminhtml/web/js/products/sync.js
+++ Extend/Warranty/view/adminhtml/web/js/products/sync.js
@@ -7,11 +7,66 @@ define(
     function($, alert, $t) {
         'use strict';
 
-        function restore(button,msg,cancel) {
+        var currentBatchesProcessed = 0;
+        var totalBatches = 0;
+        var shouldAbort = false;
+        var synMsg = $("#sync-msg");
+        var cancelSync = $("#cancel_sync");
+
+        function restore(button) {
             button.text('Sync Products');
             button.attr("disabled", false);
-            msg.show();
-            cancel.hide();
+            synMsg.show();
+            cancelSync.hide();
+            currentBatchesProcessed = 0;
+            totalBatches = 0;
+            shouldAbort = false;
+        }
+
+        async function syncAllProducts(url, button){
+            do {
+                var data = await batchBeingProcessed(shouldAbort, url).then(data => {
+                    return data;
+                }, data => {
+                    return {
+                        'totalBatches': 0,
+                        'currentBatchesProcessed': 1
+                    };
+                }).catch(e => {
+                    console.log(e);
+                });
+                currentBatchesProcessed = data.currentBatchesProcessed;
+                totalBatches = data.totalBatches;
+            } while (currentBatchesProcessed <= totalBatches);
+            restore(button);
+        }
+
+        function batchBeingProcessed(shouldAbort, url){
+            if(!shouldAbort){
+                return new Promise((resolve, reject) => {
+                    $.get({
+                        url : url,
+                        dataType: 'json',
+                        async: true,
+                        data: {
+                            currentBatchesProcessed: currentBatchesProcessed
+                        },
+                        success:function(data){
+                            resolve(data)
+                        },
+                        error:function(data){
+                            reject(data);
+                        }
+                    })
+                })
+            } else {
+                return new Promise((resolve, reject) => {
+                    resolve({
+                        'totalBatches': 0,
+                        'currentBatchesProcessed': 1
+                    });
+                })
+            }
         }
 
         $.widget('extend.productSync', {
@@ -26,13 +81,15 @@ define(
 
             _bind: function() {
                 $(this.element).click(this.syncProducts.bind(this));
+                var cancelSync = $("#cancel_sync");
+                $(cancelSync).bind( "click", function() {
+                    shouldAbort = true
+                });
 
             },
             syncProducts: function(event) {
                 event.preventDefault();
                 var button =  $(this.element);
-                var synMsg = $("#sync-msg");
-                var cancelSync = $("#cancel_sync");
 
                 button.text('Sync in progress...');
                 button.attr("disabled", true);
@@ -40,26 +97,9 @@ define(
                 synMsg.hide();
                 cancelSync.show();
 
+                syncAllProducts(this.options.url, button);
 
-                var ajaxCall = $.get({
-                    url : this.options.url
-                })
-                    .done(function(data) {
-                        restore(button,synMsg,cancelSync)
-                    })
-                    .fail(function(jqXHR, textStatus, errorThrown) {
-                        restore(button,synMsg,cancelSync)
-                        if(jqXHR.aborted){
-                            return;
-                        }
-                    });
-
-                $(cancelSync).bind( "click", function() {
-                    ajaxCall.abort();
-                });
-            },
-
-
+            }
         });
 
         return $.extend.productSync;
