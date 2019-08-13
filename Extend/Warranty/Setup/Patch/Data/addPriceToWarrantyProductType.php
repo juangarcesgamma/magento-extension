ca99e65103c063fcea6a1e41c8e2934afcfded90
Christopher Delgado <christopher.delgado@gammapartners.com>
2019-08-13 16:41:57 -0600

    [HEL-167] Warranty displayed in the cart page

---
+++ Extend/Warranty/Setup/Patch/Data/addPriceToWarrantyProductType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Extend\Warranty\Setup\Patch\Data;
+
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Eav\Setup\EavSetupFactory;
+
+class addPriceToWarrantyProductType implements DataPatchInterface
+{
+
+    protected $moduleDataSetup;
+    protected $eavSetupFactory;
+
+    public function __construct
+    (
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    )
+    {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    public function getAliases()
+    {
+        return [];
+    }
+
+    public function apply()
+    {
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        //associate these attributes with new product type
+        $fieldList = [
+            'price',
+            'minimal_price',
+            'cost',
+            'tier_price'
+        ];
+
+        // make these attributes applicable to new product type
+        foreach ($fieldList as $field) {
+            $applyTo = explode(
+                ',',
+                $eavSetup->getAttribute(\Magento\Catalog\Model\Product::ENTITY, $field, 'apply_to')
+            );
+            if (!in_array(\Extend\Warranty\Model\Product\Type::TYPE_ID, $applyTo)) {
+                $applyTo[] = \Extend\Warranty\Model\Product\Type::TYPE_ID;
+                $eavSetup->updateAttribute(
+                    \Magento\Catalog\Model\Product::ENTITY,
+                    $field,
+                    'apply_to',
+                    implode(',', $applyTo)
+                );
+            }
+        }
+    }
+}
