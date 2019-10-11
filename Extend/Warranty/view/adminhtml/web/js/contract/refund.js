fb37366492c81cda1335d02aff802c858804ef02
Jose Royo <jose.royo@gammapartners.com>
2019-10-11 14:10:34 -0600

    [HEL-206] Fixing broken JS

--- Extend/Warranty/view/adminhtml/web/js/contract/refund.js
+++ Extend/Warranty/view/adminhtml/web/js/contract/refund.js
@@ -2,7 +2,6 @@ define(
     [
         'jquery',
         'Magento_Ui/js/modal/alert',
-        'Magento_Ui/js/modal/modal',
         'mage/translate'
     ],
     function ($, alert, $t) {
@@ -55,28 +54,16 @@ define(
             },
 
             refundWarranty: function (event) {
+
                 const url = this.options.url;
                 const contractId = this.options.contractId;
                 const itemId = this.options.itemId;
 
-                var modalOptions = {
-                    type: 'popup',
-                    responsive: true,
-                    innerScroll: true,
-                    buttons: [{
-                        text: 'Ok',
-                        class: '',
-                        click: function() {
-                            refund(url, contractId, itemId);
-                        }
-                    }]
-                };
-                var confirmModal = modal(modalOptions, $('#popup-modal'));
-                $('#popup-modal').modal("openModal");
 
-                // if (confirm("Are you sure?")) {
-                //     refund(url, contractId, itemId);
-                // }
+                if (confirm("Are you sure?")) {
+                    refund(url, contractId, itemId);
+                }
+
             }
         });
 
