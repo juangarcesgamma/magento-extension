246e7442595306436e73ae8e20278957b2b90390
Christopher Delgdao <christopher.delgado@gammapartners.com>
2019-10-14 11:26:05 -0600

    [HEL-222] Small fixes for invoice

--- Extend/Warranty/Plugin/Controller/Cart/Add.php
+++ Extend/Warranty/Plugin/Controller/Cart/Add.php
@@ -8,7 +8,6 @@ use Magento\Catalog\Api\ProductRepositoryInterface;
 use Extend\Warranty\Model\Product\Type as WarrantyType;
 use Magento\Checkout\Helper\Cart;
 use Magento\Framework\Exception\LocalizedException;
-use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Framework\Message\ManagerInterface;
 
@@ -57,19 +56,9 @@ class Add
 
     public function beforeExecute(SuperAdd $subject)
     {
-        $productId = $this->request->getPost('product');
-
-        try {
-            $product = $this->productRepository->getById($productId);
-        } catch (NoSuchEntityException $e) {
-            return null;
-        }
-
         $warrantyData = $this->request->getPost('warranty');
 
         if ($warrantyData) {
-
-            $warrantyData['product'] = $product->getSku();
 
             $this->searchCriteriaBuilder
                 ->setPageSize(1)->addFilter('type_id', WarrantyType::TYPE_CODE);
