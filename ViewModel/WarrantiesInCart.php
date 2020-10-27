5f1df3245813efa99d5f887006324a9016b27560
Juan Garcés <jgarces@pkglobal.com>
2020-10-27 14:10:36 -0600

    feature/HEL-298

--- ViewModel/WarrantiesInCart.php
+++ ViewModel/WarrantiesInCart.php
@@ -7,6 +7,7 @@ namespace Extend\Warranty\ViewModel;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Magento\Checkout\Model\Session;
 use Extend\Warranty\Helper\Api\Data;
+use Magento\Sales\Model\AdminOrder\Create as OrderCreate;
 
 class WarrantiesInCart implements ArgumentInterface
 {
@@ -14,27 +15,46 @@ class WarrantiesInCart implements ArgumentInterface
 
     protected $helper;
 
+    /**
+     * @var OrderCreate
+     */
+    protected $orderCreate;
+
     public function __construct
     (
         Session $checkoutSession,
-        Data $helper
+        Data $helper,
+        OrderCreate $orderCreate
     )
     {
         $this->checkoutSession = $checkoutSession;
         $this->helper = $helper;
+        $this->orderCreate = $orderCreate;
     }
 
-    public function hasWarranty($sku)
+    public function hasWarranty($sku, $fromAdmin = false)
     {
-        foreach ($this->checkoutSession->getQuote()->getAllVisibleItems() as $item) {
-            if ($item->getProductType() === 'warranty') {
-                if ($item->getOptionByCode('associated_product')->getValue() === $sku) {
-                    return true;
+        if ($fromAdmin) {
+            $quoteData =  $this->orderCreate->getQuote();
+            foreach ($quoteData->getAllVisibleItems() as $item) {
+                if ($item->getProductType() === 'warranty') {
+                    if ($item->getOptionByCode('associated_product')->getValue() === $sku) {
+                        return true;
+                    }
                 }
-
             }
+            return false;
+        } else {
+            foreach ($this->checkoutSession->getQuote()->getAllVisibleItems() as $item) {
+                if ($item->getProductType() === 'warranty') {
+                    if ($item->getOptionByCode('associated_product')->getValue() === $sku) {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
-        return false;
+
     }
 
     public function isDisplayOffersEnabled()
