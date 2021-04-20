0f47b51be1920cc61f3f8aed788f23843316a03b
Juan Garcés <jgarces@pkglobal.com>
2021-04-20 17:28:40 -0500

    Show Extend Version in the Extend Panel.

--- Helper/Api/Data.php
+++ Helper/Api/Data.php
@@ -5,22 +5,27 @@ namespace Extend\Warranty\Helper\Api;
 use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Framework\App\Helper\Context;
 use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Module\ModuleListInterface;
 
 class Data extends AbstractHelper
 {
     CONST BASEPATH = 'warranty/authentication/';
-
     CONST ENABLE_PATH = 'warranty/enableExtend/';
+    CONST MODULE_NAME = 'Extend_Warranty';
 
     protected $scopeConfig;
+
+    protected $moduleList;
 
     public function __construct
     (
         Context $context,
-        ScopeConfigInterface $config
+        ScopeConfigInterface $config,
+        ModuleListInterface $moduleList
     )
     {
         $this->scopeConfig = $config;
+        $this->moduleList = $moduleList;
         parent::__construct($context);
     }
 
@@ -56,5 +61,9 @@ class Data extends AbstractHelper
     public function isRefundEnabled() {
         $path = self::ENABLE_PATH. 'enableRefunds';
         return $this->scopeConfig->isSetFlag($path);
+    }
+
+    public function getVersion() {
+        return $this->moduleList->getOne(self::MODULE_NAME)['setup_version'];
     }
 }
